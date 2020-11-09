f7c485780000ce3cb84d5eed4857d6f639801973
Eduardo Parra <helloeduardo@icloud.com>
2020-11-09 11:22:50 -0700

    Add flash message

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
   def destroy
     Appointment.where(doctor_id: params[:id], patient_id: params[:patient_id]).destroy_all
-
+    flash[:notice] = "The patient was successfully removed"
     redirect_to "/doctors/#{params[:id]}"
   end
 end
